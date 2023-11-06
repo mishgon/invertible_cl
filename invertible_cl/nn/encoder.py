79c4f3e5eca6b01355ac01b8b6e6e99cd89d7eed
mgoncharov <m.goncharov@ira-labs.com>
2023-11-06 15:48:48 +0300

    colab notebook

--- invertible_cl/nn/encoder.py
+++ invertible_cl/nn/encoder.py
@@ -5,22 +5,27 @@ import torch.nn as nn
 from .resnet import resnet18, resnet50, adapt_to_cifar10
 
 
-EncoderArchitecture = Literal['resnet18', 'resnet18_cifar10', 'resnet50', 'resnet50_cifar10']
+EncoderArchitecture = Literal[
+    'resnet18',
+    'resnet18_32x32',
+    'resnet50',
+    'resnet50_32x32'
+]
 
 
 def encoder(architecture: EncoderArchitecture, **kwargs) -> Tuple[nn.Module, int]:
-    if architecture in ['resnet18', 'resnet18_cifar10']:
+    if architecture in ['resnet18', 'resnet18_32x32']:
         encoder = resnet18(**kwargs)
         encoder.fc = nn.Identity()
         embed_dim = 512
-    elif architecture in ['resnet50', 'resnet50_cifar10']:
+    elif architecture in ['resnet50', 'resnet50_32x32']:
         encoder = resnet50(**kwargs)
         encoder.fc = nn.Identity()
         embed_dim = 2048
     else:
         raise ValueError(f'``encoder={encoder}`` is not supported')
 
-    if architecture in ['resnet18_cifar10', 'resnet50_cifar10']:
+    if architecture in ['resnet18_32x32', 'resnet50_32x32']:
         encoder = adapt_to_cifar10(encoder)
     
     return encoder, embed_dim
